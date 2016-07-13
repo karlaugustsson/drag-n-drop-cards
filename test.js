501630bca27e07c2d824115cd389c0ef7f376976
Karl Augustsson <karl.augustsson@gmail.com>
2016-07-13 18:13:23 +0200

    cards can be dragged and placed vertically with quite satisfying results in chrome

--- test.js
+++ test.js
@@ -3,24 +3,51 @@ window.onload = () => {
 	let cardtest = document.getElementsByClassName("card");
 	let dragged_item = null;
 	let hideElement = (el) => appendClass(el,"hide");
+	let showElement = (el) => removeClass(el,"hide");
 	let hasClass = (el,c) => el.classList.contains(c);
 	let appendClass = (el,c) => el.classList.add(c);
 	let removeClass = (el,c) => el.classList.remove(c);
+	let sameId = (el,el2) => {return el.id == el2.id};
 
 
 	for(let i  = 0 ; i < cardtest.length ; i++){
 		cardtest[i].addEventListener("dragover",()=>{});
 		cardtest[i].addEventListener("dragstart" , (e) => {dragged_item = e.target;startDragging(e,true),appendClass(e.target,"placeholder")});
 		cardtest[i].addEventListener("dragenter" , (e) => {hideElementIfNotSameId(dragged_item,e.target)})
-		cardtest[i].addEventListener("dragleave" ,()=>{});
+		cardtest[i].addEventListener("dragleave" ,(e)=>{movePlaceholderNextToElement(e)});
 		cardtest[i].addEventListener("dragend" , (e) => { removeClass(e.target,"placeholder")});
 		cardtest[i].addEventListener("drop" , (e) => { dropItemIntoContainer(e,card_container)});
 	}
 	function hideElementIfNotSameId(el1,el2){
-		
-		if(el1.id != el2.id ){
+
+		if(!sameId(el1,el2) ){
 			hideElement(el1);
 		}
+	}
+	function placeElementBefore(el,el2){
+		el.parentNode.insertBefore(el,el2);
+	}
+	function placeElementAfter(el,el2){
+		el2.parentNode.insertBefore(el, el2.nextSibling);
+	}
+	function movePlaceholderNextToElement(e){
+		let victim = e.target;
+		victimLeft = victim.offsetLeft;
+		victimRight = victimLeft + victim.offsetWidth;
+		
+		if( sameId(victim,dragged_item)){
+			return false;
+		}
+
+		if( Math.abs(victimLeft  - e.clientX) < (victimRight - e.clientX)){
+			placeElementBefore(dragged_item,victim);
+			
+
+		}else{
+			placeElementAfter(dragged_item,victim,card_container);
+		}
+		showElement(dragged_item);
+
 	}
 	function removeElementsWithClass(c){
 		let el = document.getElementsByClassName(c);
